3ec6ce98021a1f68610c2f6fa78f7678efe500e4
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-18 18:09:53 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
+sudo apt install gedit -y -qq
 
 sudo useradd yoda
 #create dragobah stage
@@ -9,7 +10,7 @@ sudo echo 'use ls to find out the subdirectory which is your next destination' >
 #dragobah room 2
 sudo echo 'use ls to find out the subdirectory which is your next destination. Yes it is a second time, just to be sure.' > /home/yoda/desktop/dragobah/b/readme.txt
 #mvoe from dragobah to bespin
-sudo echo 'your next destination is /home/yoda/desktop/bespin.' > /home/yoda/desktop/dragobah/b/c/readme.txt
+sudo echo 'your next destination is /home/yoda/desktop/bespin' > /home/yoda/desktop/dragobah/b/c/readme.txt
 
 #create bespin rooms
 sudo mkdir -p /home/yoda/desktop/bespin/cloud1/cloud4
@@ -29,7 +30,7 @@ sudo mkdir -p /home/yoda/desktop/naboo/veruna/jarbinks/Jarjarbins/jarJar/JarJarb
 sudo mkdir -p /home/yoda/desktop/naboo/rori
 
 sudo echo 'There is a file within this directory (and subdirectories) called padme.txt. Open padme.txt for your next clue.' > /home/yoda/desktop/naboo/readme.txt
-sudo echo 'Find a subdirectory back on /yoda/desktop/naboo with the name: jarjarbinks. The folder name may contain uppercase letters. Navigate to that folder and open the readme in that folder to gain a clue to your next destination.' > /home/yoda/desktop/naboo/onoan/1/2/3/4/padme.txt
+sudo echo 'Find a subdirectory back on /home/yoda/desktop/naboo with the name: jarjarbinks. The folder name may contain uppercase letters. Navigate to that folder and open the readme in that folder to gain a clue to your next destination.' > /home/yoda/desktop/naboo/onoan/1/2/3/4/padme.txt
 sudo echo 'Well done! Now make your way to /home/yoda/desktop/naboo/rori' > /home/yoda/desktop/naboo/veruna/jarbinks/Jarjarbins/jarJar/JarJarbiks/JarjarBinKs/readme.txt
 #create readmes for all other jarjarbink folders
 sudo echo 'Not this' > /home/yoda/desktop/naboo/veruna/jarbinks/Jarjarbins/jarJar/JarJarbiks/JarjarBinKs/jarBinkjar.readme.txt
@@ -43,7 +44,7 @@ sudo echo 'Not this' > /home/yoda/desktop/naboo/veruna/readme.txt
 sudo echo 'Use grep to find the word starfighter in amidala.txt. Your next clue is in the same line.' > /home/yoda/desktop/naboo/rori/readme.txt
 sudo echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26 use grep to find the word starfighter in palpatine.txt. May contain uppercase letters. Your next clue is within 5 lines of starfighter\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40" > /home/yoda/desktop/naboo/rori/amidala.txt
 sudo echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19sTarFigHter\n20\n21\n22\n23 The clue to your next destination is on the 8th line from the bottom of pooja.txt\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40" > /home/yoda/desktop/naboo/rori/palpatine.txt
-sudo echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33 Please navigate to /yoda/desktop/endor\n34\n35\n36\n37\n38\n39\n40" > /home/yoda/desktop/naboo/rori/pooja.txt
+sudo echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33 Please navigate to /home/yoda/desktop/endor\n34\n35\n36\n37\n38\n39\n40" > /home/yoda/desktop/naboo/rori/pooja.txt
 
 #create endor 
 sudo mkdir -p /home/yoda/desktop/endor
